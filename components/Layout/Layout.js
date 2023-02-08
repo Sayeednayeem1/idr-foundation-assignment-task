118666be636c0188e73bdad1ab94b5913e438e1a
Abu Sayeed <sayeed@pop-os.localdomain>
2023-02-08 13:43:57 +0600

    added bg gradient

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -4,7 +4,7 @@ import Navbar from '../Navbar/Navbar';
 
 const Layout = ({ children }) => {
     return (
-        <div>
+        <div className='bg-gradient-to-r from-[#F9ECED] via-[#FEE5F9] to-[#F2EEF7]'>
             <Navbar></Navbar>
             <main>{children}</main>
             <Footer></Footer>
