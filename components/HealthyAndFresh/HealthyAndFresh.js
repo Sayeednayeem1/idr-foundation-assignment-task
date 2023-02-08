766cf10413431ae724369adb35d1de4157e8bfa4
Abu Sayeed <sayeed@pop-os.localdomain>
2023-02-08 11:28:53 +0600

    polished HealthyAndFresh component

--- components/HealthyAndFresh/HealthyAndFresh.js
+++ components/HealthyAndFresh/HealthyAndFresh.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const HealthyAndFresh = () => {
     return (
-        <div>
+        <div className='mb-[120px] px-6 md:px-[110px] mt-[120px]'>
             <div className="flex flex-col md:flex-row justify-between items-center">
-                <div className="p-4 w-full md:w-1/2">
-                    <h2>Healthy in side <br /> fresh out side</h2>
+                <div className="p-4 w-full md:w-[60%] ">
+                    <h2 className='text-[#262524] font-normal text-5xl'>Healthy in side <br /> fresh out side</h2>
                 </div>
-                <div className="p-4 w-full md:w-1/2">
-                    <p>Exercise is a very important need for our body. Health and fitness will be obtained if you can do regular exercise and run a healthy routine. Even at home we still have to be diligent in exercising, either alone or with your friends at home. Healthy life makes you more excited to live the day</p>
+                <div className="p-4 w-full md:w-[40%] opacity-50">
+                    <p>Exercise is a very important need for our body. Health and fitness will be obtained <br/> if you can do regular exercise and run a healthy routine. Even at home we still have <br/> to be diligent in exercising, either alone or with your friends at home. Healthy life <br/> makes you more excited to live the day</p>
                 </div>
             </div>
 
